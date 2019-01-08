cc884cc843a5ba405f61492c91ddce775b62e123
Taylor Schmutz <taylorschmutz@gmail.com>
2019-01-07 19:49:58 -0800

    Add skeleten to dynamic and static arrays

---
+++ arrays/static_array.rb
@@ -0,0 +1,15 @@
+class StaticArray
+    def initialize(length)
+    end
+  
+    # O(1)
+    def [](index)
+    end
+  
+    # O(1)
+    def []=(index, value)
+    end
+  
+    protected
+    attr_accessor :store
+  end
