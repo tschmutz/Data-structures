c4c4b0b2c67a86de0bbf825631b1fca1a4a1000a
Taylor Schmutz <taylorschmutz@gmail.com>
2019-01-12 12:40:29 -0800

    Add queue skeleton

---
+++ arrays/queue_with_max.rb
@@ -0,0 +1,29 @@
+require_relative 'ring_buffer'
+
+class QueueWithMax
+  attr_reader :store, :maxque
+
+  def initialize
+
+  end
+
+  def enqueue(el)
+
+  end
+
+  def dequeue
+
+  end
+
+  def max
+
+  end
+
+  def update_maxque(el)
+
+  end
+
+  def length
+
+  end
+end
