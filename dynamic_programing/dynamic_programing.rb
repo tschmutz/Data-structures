8e0ef115bf8ca45bb581d38fb8de3d33041324fa
Taylor Schmutz <taylorschmutz@gmail.com>
2019-08-01 12:33:18 -0700

    Finish knapsack

--- dynamic_programing/dynamic_programing.rb
+++ dynamic_programing/dynamic_programing.rb
@@ -72,9 +72,25 @@ class DynamicProgramming
     end 
 
 
-    def knapsack_table
+    def knapsack_table(values, weights, capacity)
         solution_table = []
-
+        (0..capacity).each do |i|
+            solution_table[i] = []
+       
+            (0..weights.length - 1).each do |j|
+                if i.zero?
+                    solution_table[i][j] = 0
+                elsif j.zero?
+                    solution_table[i][j] = weights[0] > i ? 0 : values[0]
+                else
+                    option1 = solution_table[i][j - 1]
+                    option2 = i < weights[j] ? 0 : solution_table[i - weights[j]][j - 1] + values[j]
+        
+                    optimum = [option1, option2].max
+                    solution_table[i][j] = optimum
+                end
+            end
+        end 
 
         solution_table
     end
