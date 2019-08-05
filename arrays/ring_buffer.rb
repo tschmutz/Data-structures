89db4644272f94ad8e21650344e684cbe9a2479a
Taylor Schmutz <taylorschmutz@gmail.com>
2019-08-04 21:23:37 -0700

    Error

--- arrays/ring_buffer.rb
+++ arrays/ring_buffer.rb
@@ -22,7 +22,7 @@ class RingBuffer
 
   # O(1)
   def pop
-    raise "index out of bounds" if (length == 0)
+    raise "Index out of bounds" if (length == 0)
 
     val, self[length - 1] = self[length - 1], nil
     self.length -= 1
