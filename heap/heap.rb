42c48cf1c85f750cd39c3af4c8bf1c764295bad6
Taylor Schmutz <taylorschmutz@gmail.com>
2019-08-11 18:27:09 -0700

    Add Child indicies class method

--- heap/heap.rb
+++ heap/heap.rb
@@ -13,4 +13,13 @@ class BinaryMinHeap
     def peek
         @store[0]
     end
+
+    def self.child_indices(len, parent_index)
+        children = []
+        child_one = (parent_index * 2) + 1
+        child_two = (parent_index * 2) + 2
+        children << child_one if child_one < len
+        children << child_two if child_two < len
+        children
+      end
 end
